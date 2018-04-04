ade2fedbb45f4a936dd35f0abddf41b174a2de1c
caoyouming_centos <18729157650@163.com>
2018-04-04 17:13:14 +0800

    常见排序代码

---
+++ sort/bubble_sort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#define SIZE 8
+ 
+void bubble_sort(int a[], int n);
+ 
+void bubble_sort(int a[], int n)
+{
+    int i, j, temp;
+    for (j = 0; j < n - 1; j++)
+        for (i = 0; i < n - 1 - j; i++)
+        {
+            if(a[i] > a[i + 1])
+            {
+                temp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = temp;
+            }
+        }
+}
+ 
+int main()
+{
+    int number[SIZE] = {95, 45, 15, 78, 84, 51, 24, 12};
+    int i;
+    bubble_sort(number, SIZE);
+    for (i = 0; i < SIZE; i++)
+    {
+        printf("%d\t", number[i]);
+    }
+    printf("\n");
+}
