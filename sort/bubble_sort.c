52a21a1970522829811c8d98a767211e4b0ba0a3
caoyouming <943844391@qq.com>
2018-04-07 16:29:41 +0800

    commit

--- sort/bubble_sort.c
+++ sort/bubble_sort.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
-#define SIZE 8
+#define SIZE 10
  
- 
-void bubble_sort(int a[], int n)
-{
+/**
+ * bubble_sort()
+ * 冒泡排序
+ */ 
+void bubble_sort(int a[], int n){
     int i, j, temp;
     for (j = 0; j < n - 1; j++)
-        for (i = 0; i < n - 1 - j; i++)
-        {
-            if(a[i] > a[i + 1])
-            {
+        for (i = 0; i < n - 1 - j; i++){
+            if(a[i] > a[i + 1]){
                 temp = a[i];
                 a[i] = a[i + 1];
                 a[i + 1] = temp;
             }
         }
 }
- 
-int main()
-{
-    int number[SIZE] = {95, 45, 15, 78, 84, 51, 24, 12};
-    int i;
-    bubble_sort(number, SIZE);
-    for (i = 0; i < SIZE; i++)
-    {
+/**
+ *主函数
+ */
+int main(){
+    int number[SIZE];
+    int i,n=0;
+    printf("Input:");
+    for(i=0;;i++){
+        scanf("%d",&number[i]);
+        n++;
+        if(getchar()=='\n')break;
+    }
+    printf("Before sort:");
+    for (i = 0; i < n; i++){
+        printf("%d\t", number[i]);
+    }
+    printf("\n");
+    //int number[SIZE] = {95, 45, 15, 78, 84, 51, 24, 12};
+    bubble_sort(number, n);
+    printf("After sort:");
+    for (i = 0; i < n; i++){
         printf("%d\t", number[i]);
     }
     printf("\n");
