021827245cdc223709d20eade66d122b9ce7c4a1
caoyouming <943844391@qq.com>
2018-04-08 22:09:02 +0800

    commit

--- sort/quick_sort.c
+++ sort/quick_sort.c
@@ -13,11 +13,15 @@ int main(){
 
 
 int quick_sort(int s[],int left,int right){
+    if(left < right){
+        int i = 1,j = right;
+        int x = s[];
+        while(i < j){
+            
+        }
 
 
-
-
-
+    } 
 
 
 }
