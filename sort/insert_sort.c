9cd7757d921b2829598ac547dbc6b1116d984f51
caoyouming <943844391@qq.com>
2018-04-08 22:32:29 +0800

    commit

--- sort/insert_sort.c
+++ sort/insert_sort.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
+#define SIZE 100
 
 int insertsort(int a[],int n)  
 {  
@@ -23,9 +26,36 @@ int insertsort(int a[],int n)
 }
 
 int main(){
-    int i=0,length=0;
-    int a[]={2,3,1,7,5,6};
-    length = sizeof(a)/sizeof(a[0]);
-    insertsort(a,length);
-    return 0;
+    int length=0;
+    clock_t start,finish;
+    double totaltime;
+    start=clock();
+
+    int s[SIZE];
+    int i,n=0;
+    printf("Input:");
+    for(i=0;;i++){
+        scanf("%d",&s[i]);
+        n++;
+        if(getchar()=='\n')break;
+    }
+    printf("Before sort:");
+    for (i = 0; i < n; i++){
+        printf("%d\t", s[i]);
+    }
+    printf("\n");
+
+    length = sizeof(s)/sizeof(s[0]);
+    insertsort(s,length);
+    printf("After sort:");
+    for (i = 0; i < n; i++){
+        printf("%d\t", s[i]);
+    }
+    printf("\n");
+    /********************************/
+   finish=clock();
+   totaltime=(double)(finish-start)/CLOCKS_PER_SEC;
+
+   printf("\n程序运行的时间为: %.f 秒\n",totaltime);
+
 }
